4bca1a577d8aaf622f3def39426c37a8c7b56684
davidsmith3 <david.smith3@outlook.com>
2014-12-25 17:36:10 -0500

    initial commit

---
+++ MagicSquares.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MagicSquares.Test {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
