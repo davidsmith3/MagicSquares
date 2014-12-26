66c23cebe642c3f4f942bfbc3ce0b05f96c59135
davidsmith3 <david.smith3@outlook.com>
2014-12-25 21:01:02 -0500

    implemented generator

---
+++ MagicSquares.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagicSquares.Domain;
+
+namespace MagicSquares.Console {
+    class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine(new MagicSquare(9, "llur").ToString());
+            System.Console.ReadLine();
+        }
+    }
+}
